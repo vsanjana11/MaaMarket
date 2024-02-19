c15b0be213a63adca83adf2250447ae04dd39150
Rachana Marri <marri.rachana20@gmail.com>
2024-02-19 15:55:51 -0600

    Spring Templete - RM

---
+++ MaaMarket/src/main/java/com/MedicalFancy/MaaMarket/MaaMarketApplication.java
@@ -0,0 +1,13 @@
+package com.MedicalFancy.MaaMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaaMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaaMarketApplication.class, args);
+	}
+
+}
---
+++ MaaMarket/src/test/java/com/MedicalFancy/MaaMarket/MaaMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MedicalFancy.MaaMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaaMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
