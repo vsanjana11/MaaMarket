e191caff229fea4b912b42e81e9d29b36bfca04b
vsanjana11 <bviswan@ilstu.edu>
2024-02-22 14:21:07 -0600

    new template & mysql config SV

---
+++ maamarket/src/main/java/com/medical/maamarket/MaamarketApplication.java
@@ -0,0 +1,13 @@
+package com.medical.maamarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaamarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaamarketApplication.class, args);
+	}
+
+}
---
+++ maamarket/src/test/java/com/medical/maamarket/MaamarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medical.maamarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaamarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
